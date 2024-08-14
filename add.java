2974477470b1e336283db4913aec032ad6b8f037
Anitha Mayilvaganan <anithavaganan@gmail.com>
2024-08-14 23:06:53 +0530

    Updated add.java program

--- add.java
+++ add.java
@@ -1,5 +1,10 @@
+public class addition{
+    public static void main(String[] args){
 int a = 10;
 int b = 20;
-int sum = a +b;
-System.out.println("sum");
+int c = 25;
+int sum = a +b+c;
+System.out.println("sum of numbers:" +sum);
+}
+}
 
